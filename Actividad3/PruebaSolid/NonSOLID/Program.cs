4517963d9b2192f85732c1cb0b258ef423573e6b
gamousquesORT <mousques@ort.edu.uy>
2024-10-31 19:57:04 -0300

    agregue ejercico prueba3

---
+++ Actividad3/PruebaSolid/NonSOLID/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NonSOLID;
+
+public class Program
+{
+    public static void Main()
+    {
+        Product foodProduct = new Product("Bread", "Food", 100);
+        Product electronicsProduct = new Product("Laptop", "Electronics", 1000);
+
+        TaxCalculator calculator = new TaxCalculator();
+
+        decimal taxForFoodProduct = calculator.CalculateTax(foodProduct);
+        decimal taxForElectronicProduct = calculator.CalculateTax(electronicsProduct);
+        
+        Console.WriteLine($"Tax for {foodProduct.Name}: {taxForFoodProduct}");
+        Console.WriteLine($"Tax for {electronicsProduct.Name}: {taxForElectronicProduct}");
+    }
+}
